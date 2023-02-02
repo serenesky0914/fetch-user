2bdf022b9f39b1fe15b3141e1594e23cd6dcfa20
serenesky0914 <kousai914@gmail.com>
2023-02-03 02:42:25 +0900

    style: add add style in homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,14 +15,37 @@ export default function RichObjectTreeView() {
   );
 
   return (
-    <TreeView
-      aria-label="rich object"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpanded={["root"]}
-      defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 110, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
-    >
-      {renderTree(data)}
-    </TreeView>
+    <>
+      <div className="container mx-auto px-4 mt-50 flex justify-evenly flex-col md:flex-row w-screen m-3 flex-auto">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="p-30">
+            <h1 className="text-3xl font-bold mb-10">Please enter your name and pick the Sectors you are currently involved in.</h1>
+            <div className="name-input mb-10">
+              <label className="text-gray-700 text-sm font-bold mb-2">Name : </label>
+              <input className="name"></input>
+            </div>
+            <div className="sectors mb-10">
+              <label>Sectors : </label>
+              <TreeView
+                aria-label="rich object"
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpanded={["root"]}
+                defaultExpandIcon={<ChevronRightIcon />}
+                sx={{ height: 110, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+              >
+                {renderTree(data)}
+              </TreeView>
+            </div>
+            <div className="agree mb-10">
+              <input type='checkbox'></input>
+              <label>Agree to terms</label>
+            </div>
+            <div className="actions">
+              <button className="">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
