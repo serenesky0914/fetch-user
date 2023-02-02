81a5bc1c0926bc25a57a1647f31973810a7d52eb
serenesky0914 <kousai914@gmail.com>
2023-02-02 23:32:36 +0900

    feat: create mockupdata

---
+++ src/mock/mockdata.js
@@ -0,0 +1,28 @@
+export const data = {
+  id: "root",
+  name: "Manufacturing",
+  children: [
+    {
+      id: "1",
+      name: "Construction materials",
+    },
+    {
+      id: "3",
+      name: "Electronics and Optics",
+    },
+    {
+      id: "4",
+      name: "Food and Beverage",
+    },
+    {
+      id: "5",
+      name: "Electronics and Optics",
+      children: [
+        {
+          id: "6",
+          name: "Bakery confectionery products",
+        },
+      ],
+    },
+  ],
+};
